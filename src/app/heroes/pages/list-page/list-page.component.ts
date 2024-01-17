682a8c8809c5808961051ea7333f8f2edf381a76
FranciscoJimenezJimenez24 <franjimenezjimenez24@gmail.com>
2024-01-17 21:10:21 +0100

    Update

--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroService } from '../../services/hero.service';
 
 @Component({
   selector: 'app-list-page',
@@ -6,6 +8,22 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit{
 
+  //variable para almacenar el listado de héroes
+
+  //inyectar el servicio
+
+  @Input()
+  public listaHeroes:Hero[]=[]
+  constructor(private heroService:HeroService){}
+
+
+  ngOnInit(): void {
+    this.heroService.getHeroes().subscribe(
+      hero =>{
+        this.listaHeroes=hero
+      }
+    )
+  }
 }
