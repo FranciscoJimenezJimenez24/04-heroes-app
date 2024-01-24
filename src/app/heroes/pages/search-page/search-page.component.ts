5688f81c1f32f168e054e584c6effdf73202e0c0
FranciscoJimenezJimenez24 <franjimenezjimenez24@gmail.com>
2024-01-24 21:14:05 +0100

    UPDATE

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroService } from '../../services/hero.service';
+import { MatAutocompleteActivatedEvent, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-page',
@@ -10,5 +13,27 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class SearchPageComponent {
 
   public searchInput = new FormControl('');
+  public heroes:Hero[]=[];
+  public selectedHero?:Hero;
+
+  constructor(private heroesService: HeroService){}
+
+  public searchHero(){
+    const value:string =this.searchInput.value || '';
+    this.heroesService.getSuggestions(value).subscribe(
+      heroes => this.heroes = heroes
+    )
+  }
+
+  public onSelectedOption(event: MatAutocompleteSelectedEvent){
+    if (!event.option.value){
+      this.selectedHero=undefined;
+      return;
+    }
+
+    const hero: Hero=event.option.value;
+    this.searchInput.setValue(hero.superhero);
+    this.selectedHero=hero;
+  }
 
 }
